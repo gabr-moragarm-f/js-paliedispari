f58e5b71ee7999ce44f9d2fd1da5b4a5494475a9
Gabriel Moraga <agarom97@gmail.com>
2021-01-07 14:27:57 +0100

    Prima parte esercizio completa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+// Funzione che verifica se una parola è palindroma ----------------------------
+function checkPalindrome(word) {
+  var reverseWord = '';
+
+  // Ciclo che inverte la parola input -----------------------------------------
+  for (var i = word.length - 1; i >= 0; i --) {
+    reverseWord += word[i];
+  }
+
+  // Verifica che la parola input sia uguale a quella invertita ----------------
+  if (reverseWord === word) {
+    return true;
+  }else {
+    return false;
+  }
+}
+
 var palindromaButton = document.getElementById('palindroma-button');
 
+var wordInput;
+
 palindromaButton.addEventListener('click', function() {
-  alert('ok')
+  wordInput = prompt('Digita la parola da verificare se è palindroma.');
+
+  if (checkPalindrome(wordInput)) {
+    alert('La parola ' + wordInput + ' è palindroma');
+  } else {
+    alert('La parola ' + wordInput + ' NON è palindroma');
+  }
 })
